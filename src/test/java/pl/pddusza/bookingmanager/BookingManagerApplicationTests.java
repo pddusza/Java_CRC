5c4fac799534797d42bad22eed8eb9eda98cb545
pdusza <piotrdusza03@gmail.com>
2026-05-27 23:04:03 +0200

    initialize spring boot project

---
+++ src/test/java/pl/pddusza/bookingmanager/BookingManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pddusza.bookingmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
